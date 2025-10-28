5e416096f5c6f39938c6319d2860cbb502ba55d5
Juliano Ostroski <juliano.ostroski@gmail.com>
2025-10-28 16:34:24 -0400

    fix: hook file fixing test

--- hook.php
+++ hook.php
@@ -9,10 +9,10 @@ function plugin_dashboard_install(){
         (`type` INTEGER , `id` INTEGER, `quant` INTEGER, PRIMARY KEY (`id`))
 						ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci; ";
 						
-        $DB->query($query) or die("error creating glpi_plugin_dashboard_count " . $DB->error());
+        $DB->doQuery($query);
         
         $insert = "INSERT INTO glpi_plugin_dashboard_count (type,quant) VALUES ('1','1')";
-        $DB->query($insert);
+        $DB->doQuery($insert);
      } 	
     
 //map
@@ -26,7 +26,7 @@ function plugin_dashboard_install(){
 	  PRIMARY KEY (`id`,`entities_id`)) 
 	  ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;";
 	
-		$DB->query($query_map) or die("error creating table glpi_plugin_dashboard_map " . $DB->error());
+		$DB->doQuery($query_map);
 		
 	}	
 	
@@ -45,7 +45,7 @@ function plugin_dashboard_install(){
 	  KEY `name_2` (`name`,`users_id`)) 
 	  ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ; ";
 	
-	  $DB->query($query_conf) or die("error creating table glpi_plugin_dashboard_config " . $DB->error());
+	  $DB->doQuery($query_conf);
 	 
 	}	
 
@@ -53,28 +53,28 @@ function plugin_dashboard_install(){
 	if ($DB->TableExists("glpi_plugin_dashboard_count")) {
 		
 		$query_alt = "ALTER TABLE `glpi_plugin_dashboard_count` DROP PRIMARY KEY, ADD PRIMARY KEY(`type`,`id`); ";		
-		$DB->query($query_alt) or die("error update table glpi_plugin_dashboard_count primary key " . $DB->error());	
+		$DB->doQuery($query_alt);	
 	}
 	
 	
 	if ($DB->TableExists("glpi_plugin_dashboard_config")) {
 		
 		$query_alt = "ALTER TABLE glpi_plugin_dashboard_config MODIFY value varchar(125); ";				
-		$DB->query($query_alt) or die("error alter table glpi_plugin_dashboard_config value size" . $DB->error());
+		$DB->doQuery($query_alt);
 		
 		//Config entities
 		$query_ent = "SELECT users_id FROM glpi_plugin_dashboard_config WHERE name = 'entity' AND value = '-1' ";		
-		$result = $DB->query($query_ent) or die("error alter table glpi_plugin_dashboard_config value size" . $DB->error());		
+		$result = $DB->request($query_ent);		
 		
-		while ($row = $DB->fetchAssoc($result)) {
+		foreach ($result as $row) {
 			$query = "UPDATE glpi_plugin_dashboard_config SET value = '' WHERE name = 'entity' AND users_id = ".$row['users_id']." ";
-			$DB->query($query) or die("error updating table glpi_plugin_dashboard_config entity value" . $DB->error());
+			$DB->doQuery($query);
 		}				
 	}
 	
 	if ($DB->TableExists("glpi_plugin_dashboard_map")) {	
 		$query_alt = "ALTER TABLE `glpi_plugin_dashboard_map` ADD UNIQUE (`location`); ";		
-		$DB->query($query_alt) or die("error update table glpi_plugin_dashboard_map primary key " . $DB->error());	
+		$DB->doQuery($query_alt);	
 	}	
 		
 	return true;
@@ -86,16 +86,16 @@ function plugin_dashboard_uninstall(){
 	global $DB;
 	
 	$drop_count = "DROP TABLE glpi_plugin_dashboard_count";
-	$DB->query($drop_count); 	
+	$DB->doQuery($drop_count); 	
 	
 	$drop_map = "DROP TABLE glpi_plugin_dashboard_map";
-	$DB->query($drop_map);
+	$DB->doQuery($drop_map);
 	
 	$drop_config = "DROP TABLE glpi_plugin_dashboard_config";
-	$DB->query($drop_config);
+	$DB->doQuery($drop_config);
 	
 	$restore_mode = "SET sql_mode=(SELECT CONCAT(@@sql_mode,',ONLY_FULL_GROUP_BY'));";
-	$DB->query($restore_mode);
+	$DB->doQuery($restore_mode);
 	
 	return true;
 
